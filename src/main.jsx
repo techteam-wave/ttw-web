1405bd7414545ad2b62cc783075532c3dfa155e7
Singh2112 <123933095+Singh2112@users.noreply.github.com>
2024-06-16 15:46:20 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+
+function Main() {
+  return (
+    <Router>
+      <Route exact path="/">
+        <Redirect to="https://techteam-wave.github.io/ttw-web" />
+      </Route>
+      <App />
+    </Router>
+  );
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Main />
+  </React.StrictMode>
+);
